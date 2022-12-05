aaa9e7a977171c8c160c856bded5a4878abdc3b0
d4gl4s <daglas.aitsen@gmail.com>
2022-12-05 18:02:24 +0200

    Vaheleht 2 responsive ja commentid

---
+++ js/vaheleht2.js
@@ -0,0 +1,6 @@
+/* Javascript -> Daglas Aitsen 2022 */
+
+/* Kutsudes, muudab suure pildi pildiks, mille peale vajutati */
+function muudaPilti(src) {
+  document.getElementById("piltSuurElement").src = src /* Muudab elemendi 'piltSuurElement' pildi 'src' väärtust, muutes selle funktsiooni parameetri 'src' väärtuseks */
+}
